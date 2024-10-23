1270e580b6c8d0afaaa0025f4454c2b7077db4de
Anjali01127 <167052174+Anjali01127@users.noreply.github.com>
2024-10-23 08:42:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add interactivity to the Start button
+document.getElementById('start-btn').addEventListener('click', function() {
+    // Redirect to the main practice page (topics.html)
+    window.location.href = 'topics.html'; // Replace with your actual URL
+});
